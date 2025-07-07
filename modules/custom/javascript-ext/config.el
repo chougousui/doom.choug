86f922630131abb32e389cab660fd23dfc001e61
choug <chougousui@gmail.com>
2025-07-07 15:51:26 +0900

    feat: 完善lsp-biome配置稳定性

--- modules/custom/javascript-ext/config.el
+++ modules/custom/javascript-ext/config.el
@@ -1,5 +1,15 @@
 ;;; custom/javascript-ext/config.el -*- lexical-binding: t; -*-
 
+;; web-mode中没有设置非web-mode相关(比如typescript-mode)的内容
+;; 但typescript-mode的内容影响tsx文件中的行为
+(setq typescript-indent-level 2
+      web-mode-css-indent-offset 2
+      ;; 虽然这个归css-mode管,不关javascript module的事
+      ;; 但是为了体验统一,也要一起修改
+      css-indent-offset 2
+      )
+
+;; lsp-biome在检测到 biome.json(c) 的时候会启动
 (use-package! lsp-biome
   :after lsp-mode
   :config
@@ -11,8 +21,8 @@
 ;; lsp-biome 的激活函数依赖 apheleia-formatters 变量存在
 ;; 但不巧的是 apheleia 不知道什么原因没有给它设置autoload
 ;; 不得不完整加载 apheleia
-(add-hook 'web-mode-hook
-          (lambda ()
-            (unless (boundp 'apheleia-formatters)
-              (require 'apheleia))
-            (apheleia-mode 1)))
+(dolist (hook '(web-mode-hook typescript-mode-hook js2-mode-hook css-mode-hook))
+  (add-hook hook (lambda ()
+                   (unless (boundp 'apheleia-formatters)
+                     (require 'apheleia))
+                   (apheleia-mode 1))))
