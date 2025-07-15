ea2ea6d02cb74bc775609d953c5b27ccc0375ab5
choug <chougousui@gmail.com>
2025-07-16 02:51:21 +0900

    feat: 新的claude code使用eat作为terminal后端

--- modules/custom-ai/claude-code/packages.el
+++ modules/custom-ai/claude-code/packages.el
@@ -9,3 +9,13 @@
            :files ("*.el" (:exclude "demo.gif"))
            )
   )
+
+(package! eat
+  :recipe (:type git
+           :host codeberg
+           :repo "akib/emacs-eat"
+           :files ("*.el" ("term" "term/*.el") "*.texi"
+                   "*.ti" ("terminfo/e" "terminfo/e/*")
+                   ("terminfo/65" "terminfo/65/*")
+                   ("integration" "integration/*")
+                   (:exclude ".dir-locals.el" "*-tests.el"))))
