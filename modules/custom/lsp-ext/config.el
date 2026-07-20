8dd66662069e4ce968ee25fbeb82b0cb4a5ac862
choug <chougousui@gmail.com>
2026-07-21 00:38:06 +0900

    feat: 配置eglot下的各种lsp服务器启动方式
    web-mode使用和html-mode相同的配置
    typescript-mode使用 tsc 7.0自带的lsp

--- modules/custom/lsp-ext/config.el
+++ modules/custom/lsp-ext/config.el
@@ -10,6 +10,24 @@
   )
 
 (when (modulep! :tools lsp +eglot)
+  ;; TypeScript 7.0 内置了原生LSP Server, tsc --lsp --stdio,其他保持eglot-server-programs里面的其他默认值
+  (set-eglot-client!
+   '((js-mode :language-id "javascript")
+     (js-ts-mode :language-id "javascript")
+     (tsx-ts-mode :language-id "typescriptreact")
+     (typescript-ts-mode :language-id "typescript")
+     (typescript-mode :language-id "typescript"))
+   '("tsc" "--lsp" "--stdio")
+   '("rass ts")
+   '("typescript-language-server" "--stdio"))
+
+  ;; 让web-mode使用与html-mode相同的语言服务器,并发送正确的languageId
+  (set-eglot-client!
+   '(web-mode :language-id "html")
+   '("vscode-html-language-server" "--stdio")
+   '("html-languageserver" "--stdio"))
+
+  ;; Eglot没有内置breadcrumb,启用Eglot作者提供的第三方包
   (use-package! breadcrumb
     :config
     (breadcrumb-mode 1)))
