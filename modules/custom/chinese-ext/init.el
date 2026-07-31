539c1baf691471261c236775d6fa5056fe294034
choug <chougousui@gmail.com>
2026-07-31 09:30:54 +0900

    feat: 完善windows下无编译环境时的rime支持

--- modules/custom/chinese-ext/init.el
+++ modules/custom/chinese-ext/init.el
@@ -2,6 +2,11 @@
 
 (use-package-hook! liberime
   :post-init
-  (setq liberime-user-data-dir
-        (expand-file-name "~/.config/emacs-liberime"))
+  (setq liberime-user-data-dir (expand-file-name "~/.config/emacs-liberime/"))
+  ;; Linux：推荐系统包管理器安装 librime；无需指定模块和共享数据路径，也无需禁止gcc编译
+  (when (eq system-type 'windows-nt)
+    (let ((liberime-root (expand-file-name "~/.local/opt/liberime/")))
+      (setq liberime-module-file (expand-file-name "bin/liberime-core.dll" liberime-root))
+      (setq liberime-shared-data-dir (expand-file-name "share/rime-data/" liberime-root))
+      (setq liberime-auto-build nil))) ; Windows 使用预编译 DLL，禁止调用 GCC 自动编译
   t)
