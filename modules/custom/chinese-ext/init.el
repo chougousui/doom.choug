2ffeccb765bfdc032a5f4ce2780596d8b89f7a7c
choug <chougousui@gmail.com>
2026-08-09 21:45:49 +0900

    chore: 重命名文件夹以更符合语义

--- modules/custom/chinese-ext/init.el
+++ modules/custom/chinese-ext/init.el
@@ -2,7 +2,7 @@
 
 (use-package-hook! liberime
   :post-init
-  (setq liberime-user-data-dir (expand-file-name "~/.config/emacs-liberime/"))
+  (setq liberime-user-data-dir (expand-file-name "~/.config/my-rime-schema-for-emacs/"))
   ;; Linux：推荐系统包管理器安装 librime；无需指定模块和共享数据路径，也无需禁止gcc编译
   (when (eq system-type 'windows-nt)
     (let ((liberime-root (expand-file-name "~/.local/opt/liberime/")))
